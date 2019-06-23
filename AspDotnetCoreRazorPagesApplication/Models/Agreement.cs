3d5e551c2c475e0aa83923e370cb11f67de9eabd
shinoburc <dandydot@gmail.com>
2019-06-23 15:09:13 +0900

    Add Data Annotation for Display Name.

--- AspDotnetCoreRazorPagesApplication/Models/Agreement.cs
+++ AspDotnetCoreRazorPagesApplication/Models/Agreement.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace AspDotnetCoreRazorPagesApplication.Models
 {
     public class Agreement
     {
         public int Id { set; get; }
+
+        [Display(Name = "契約名")]
         public string Name { set; get; }
-        public double Latitude { set; get; } // 緯度
-        public double Longitude { set; get; } // 経度
+
+        [Display(Name = "緯度")]
+        public double Latitude { set; get; }
+
+        [Display(Name = "経度")]
+        public double Longitude { set; get; }
     }   
 }
